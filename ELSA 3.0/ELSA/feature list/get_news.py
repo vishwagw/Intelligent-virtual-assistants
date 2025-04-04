246195e25627ebf70e2b00784063c8904dfdb198
Vishwa GW (Randy) <136871744+vishwagw@users.noreply.github.com>
2025-04-05 00:12:28 +0530

    Update get_news.py

--- ELSA 3.0/ELSA/feature list/get_news.py
+++ ELSA 3.0/ELSA/feature list/get_news.py
@@ -1,1 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import re, pyttsx3
+
+def speak(text):
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voices', voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+    engine.setProperty('rate', 110)
 
