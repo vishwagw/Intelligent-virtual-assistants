c38ba6d5fa0b793897dc3940efe4973791e76556
Vishwa GW (Randy) <136871744+vishwagw@users.noreply.github.com>
2025-04-05 00:12:50 +0530

    Update get_news.py

--- ELSA 3.0/ELSA/feature list/get_news.py
+++ ELSA 3.0/ELSA/feature list/get_news.py
@@ -10,3 +10,17 @@ def speak(text):
     engine.runAndWait()
     engine.setProperty('rate', 110)
 
+def google_search(command):
+    reg_ex = re.search('search google for (.*)', command)
+    search_for = command.split("for", 1)[1]
+    url = 'https://wwww.google.com/'
+    if reg_ex:
+        subgoogle = reg_ex.group(1)
+        url = url + 'r/' + subgoogle
+    speak('sure sir!')
+    speak(f'searching for {subgoogle}')
+    driver = webdriver.Chrome(executable_path ='driver/chromedriver.exe')
+    driver.get('https://www.google.com')
+    search = driver.find_element_by_name('g')
+    search.send_keys(str(search_for))
+    search.send_keys(Keys.RETURN)
